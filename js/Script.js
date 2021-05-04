b3c91c2c3bc07ff553114d8cddbb08013198d8e0
TarasHaiduk <72802921+TarasHaiduk@users.noreply.github.com>
2021-05-04 21:32:48 +0300

    Add files via upload

--- js/Script.js
+++ js/Script.js
@@ -1,16 +1,31 @@
-var smoothJumpUp = function() {
+var smoothJumpUp = function () {
     if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-        window.scrollBy(0,-50);
+        window.scrollBy(0, -50);
         setTimeout(smoothJumpUp, 10);
     }
 }
 
-window.onscroll = function() {
-  var scrolled = window.pageYOffset || document.documentElement.scrollTop;
-  if (scrolled > 100) {
-      document.getElementById('upbutton').style.display = 'block';
-  }
-  else {
-      document.getElementById('upbutton').style.display = 'none';
-  }
+window.onscroll = function () {
+    var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrolled > 100) {
+        document.getElementById('upbutton').style.display = 'block';
+    }
+    else {
+        document.getElementById('upbutton').style.display = 'none';
+    }
+}
+function readmore() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
 }
